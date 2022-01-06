977b2b18e31cf8b159f43af5d7c8b6c6fb33752c
NordicBeaver <nordicbeaver@gmail.com>
2022-01-06 15:58:45 +0300

    Only rerender once every frame.

--- src/components/SketchBoardCanvas.tsx
+++ src/components/SketchBoardCanvas.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { Point, Sketch } from '../domain/Sketch';
 import { useMouseDrawDetector } from '../hooks/useMouseDrawDetector';
 import { useTouchDrawDetector } from '../hooks/useTouchDrawDetector';
@@ -35,6 +35,33 @@ export default function SketchBoardCanvas({
 }: SketchBoardCanvasProps) {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
+  const animationFrameRequestRef = useRef<number | null>(null);
+
+  const sketchToRender = useRef(sketch);
+  useEffect(() => {
+    sketchToRender.current = sketch;
+  }, [sketch]);
+
+  const heightToRender = useRef(height);
+  useEffect(() => {
+    heightToRender.current = height;
+  }, [height]);
+
+  const widthToRender = useRef(width);
+  useEffect(() => {
+    widthToRender.current = width;
+  }, [width]);
+
+  const viewPortToRender = useRef(viewport);
+  useEffect(() => {
+    viewPortToRender.current = viewport;
+  }, [viewport]);
+
+  const shouldRerender = useRef(true);
+  useEffect(() => {
+    shouldRerender.current = true;
+  }, [sketch, height, width, viewport]);
+
   const { handleMouseDown, handleMouseUp, handleMouseMove } = useMouseDrawDetector({
     canvasRef: canvasRef,
     onUserStartDrawing: onUserStartDrawing,
@@ -53,50 +80,70 @@ export default function SketchBoardCanvas({
     onUserZoom: onUserZoom,
   });
 
-  useEffect(() => {
-    const xToLocal = (x: number) => ((x - viewport.x) * width) / viewport.width;
-    const yToLocal = (y: number) => ((y - viewport.y) * height) / viewport.height;
-    const widthToLocal = (w: number) => (w * width) / viewport.width;
-    const heightToLocal = (h: number) => (h * height) / viewport.height;
+  const animationFrame = useCallback(() => {
+    if (shouldRerender.current === true) {
+      shouldRerender.current = false;
 
-    const pointToLocal = (point: Point) => {
-      const result: Point = {
-        x: ((point.x - viewport.x) * width) / viewport.width,
-        y: ((point.y - viewport.y) * height) / viewport.height,
+      const sketch = sketchToRender.current;
+      const viewport = viewPortToRender.current;
+      const width = widthToRender.current;
+      const height = heightToRender.current;
+
+      const xToLocal = (x: number) => ((x - viewport.x) * width) / viewport.width;
+      const yToLocal = (y: number) => ((y - viewport.y) * height) / viewport.height;
+      const widthToLocal = (w: number) => (w * width) / viewport.width;
+      const heightToLocal = (h: number) => (h * height) / viewport.height;
+
+      const pointToLocal = (point: Point) => {
+        const result: Point = {
+          x: ((point.x - viewport.x) * width) / viewport.width,
+          y: ((point.y - viewport.y) * height) / viewport.height,
+        };
+        return result;
       };
-      return result;
-    };
 
-    if (canvasRef?.current != null) {
-      const context = canvasRef.current.getContext('2d')!;
-      context.clearRect(0, 0, width, height);
+      if (canvasRef?.current != null) {
+        const context = canvasRef.current.getContext('2d')!;
+        context.clearRect(0, 0, width, height);
 
-      // Show gray background, where the drawing is not allowed
-      context.fillStyle = '#cccccc';
-      context.fillRect(xToLocal(-width), yToLocal(-height), widthToLocal(width * 3), heightToLocal(height * 3));
-      context.fillStyle = '#ffffff';
-      context.fillRect(xToLocal(0), yToLocal(0), widthToLocal(width), heightToLocal(height));
+        // Show gray background, where the drawing is not allowed
+        context.fillStyle = '#cccccc';
+        context.fillRect(xToLocal(-width), yToLocal(-height), widthToLocal(width * 3), heightToLocal(height * 3));
+        context.fillStyle = '#ffffff';
+        context.fillRect(xToLocal(0), yToLocal(0), widthToLocal(width), heightToLocal(height));
 
-      context.save();
-      context.lineCap = 'round';
-      context.rect(xToLocal(0), yToLocal(0), widthToLocal(width), heightToLocal(height));
-      context.clip();
-      sketch.lines.forEach((line) => {
-        context.strokeStyle = `#${line.color}`;
-        context.lineWidth = (line.thickness * (width / viewport.width + height / viewport.height)) / 2;
-        context.beginPath();
-        line.segments.forEach((segment) => {
-          const fromLocal = pointToLocal(segment.from);
-          const toLocal = pointToLocal(segment.to);
-          context.moveTo(fromLocal.x, fromLocal.y);
-          context.lineTo(toLocal.x, toLocal.y);
-          context.stroke();
+        context.save();
+        context.lineCap = 'round';
+        context.rect(xToLocal(0), yToLocal(0), widthToLocal(width), heightToLocal(height));
+        context.clip();
+        sketch.lines.forEach((line) => {
+          context.strokeStyle = `#${line.color}`;
+          context.lineWidth = (line.thickness * (width / viewport.width + height / viewport.height)) / 2;
+          context.beginPath();
+          line.segments.forEach((segment) => {
+            const fromLocal = pointToLocal(segment.from);
+            const toLocal = pointToLocal(segment.to);
+            context.moveTo(fromLocal.x, fromLocal.y);
+            context.lineTo(toLocal.x, toLocal.y);
+            context.stroke();
+          });
+          context.closePath();
         });
-        context.closePath();
-      });
-      context.restore();
+        context.restore();
+      }
     }
-  }, [height, sketch, viewport, width]);
+
+    animationFrameRequestRef.current = requestAnimationFrame(animationFrame);
+  }, []);
+
+  useEffect(() => {
+    animationFrameRequestRef.current = requestAnimationFrame(animationFrame);
+    return () => {
+      if (animationFrameRequestRef.current) {
+        cancelAnimationFrame(animationFrameRequestRef.current);
+      }
+    };
+  }, [animationFrame]);
 
   return (
     <canvas
