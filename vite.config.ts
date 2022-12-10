bd19bde3c932eec98eed80cc8786b18df859d1f8
NordicBeaver <nordicbeaver@gmail.com>
2022-12-10 11:05:05 +0200

    Prepare for npm.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { build, defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [dts(), react()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
